ba1ab0c53061e954f039442abc893b62ae030dc5
mohamed-ruwaiz <98874861+mohamed-ruwaiz@users.noreply.github.com>
2022-03-20 20:06:18 +0530

    Add files via upload

---
+++ HashSample.java
@@ -0,0 +1,24 @@
+package myjava1;
+import java.util.*;
+public class HashSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      LinkedHashSet<String> h = new LinkedHashSet<String>();
+      h.add("ruwaiz");
+      h.add("mohamed");
+      h.add("ahamed");
+      h.add("al");
+      h.add("bjvsvvjn");
+      System.out.println(h);
+      
+      HashSet<String> k = new HashSet<String>();
+      k.add("ruwaiz");
+      k.add("mohamed");
+      k.add("ahamed");
+      k.add("al");
+      k.add("bjvsvvjn");
+      System.out.println(k);
+	}
+
+}
