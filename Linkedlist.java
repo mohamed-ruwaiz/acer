3747fa1caae9c7e84d3e504d40a3c85ce435ba47
mohamed-ruwaiz <98874861+mohamed-ruwaiz@users.noreply.github.com>
2022-03-29 23:47:31 +0530

    Add files via upload

---
+++ Linkedlist.java
@@ -0,0 +1,11 @@
+package myjava1;
+import java.util.*;
+public class Linkedlist {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     LinkedList<String> l1 = new LinkedList<String>();
+     l1.add(0,"hii");
+	}
+
+}
